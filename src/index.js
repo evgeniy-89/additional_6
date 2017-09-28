6e88bf94f44351dd466504588f39bd38ec28419d
Dzmitry Kazbiarovich <roman.evgeniy@gmail.com>
2017-09-28 22:47:42 +0300

    Zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function zeros(expression) {
-  // your solution
+  expression = expression.split('*');
+  let counter2 = 0;
+  let counter5 = 0;
+  let number;
+  let temp;
+
+  for (let i = 0; i < expression.length; ++i) {
+    if (expression[i].slice(-2, -1) === '!') {
+      number = parseInt(expression[i]);
+
+      for (let j = number; j > 1; j = j - 2) {
+        temp = j;
+        while (!(temp % 5)) {
+          counter5++;
+          temp = temp / 5;
+        }
+        while (!(temp % 2)) {
+          counter2++;
+          temp = temp / 2;
+        }
+      }
+    } else {
+      number = parseInt(expression[i]);
+
+      for (let j = number; j > 1; j = j - 1) {
+        temp = j;
+        while (!(temp % 5)) {
+          counter5++;
+          temp = temp / 5;
+        }
+        while (!(temp % 2)) {
+          counter2++;
+          temp = temp / 2;
+        }
+      }
+    }
+  }
+  return Math.min(counter2, counter5);
 }
